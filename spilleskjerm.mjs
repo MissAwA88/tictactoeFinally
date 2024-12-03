368cc88e7cf0094b92d26c3f8fc2ef114e4c1a29
Anne Hilde Aadnevik <aha.aadnevik@gmail.com>
2024-12-03 12:03:17 +0100

    lagre

---
+++ spilleskjerm.mjs
@@ -0,0 +1,29 @@
+// Løsninger for spillbrett: Legg inn som egen fil(?) Spillebrett som kun ren tekstformat. Legge inn en funksjon som 
+// som henter fra separert fil. / Return funksjon i [game.mjs] -> Henter spillebrett inn
+
+/* [0, 0, 0],
+   [0, 0, 0],
+   [0, 0, 0],
+
+           
+  0  |  0  |  0  
+_____|_____|_____
+     |     |     
+  0  |  0  |  0  
+_____|_____|_____
+     |     |     
+  0  |  0  |  0  
+     |     |   
+
+*/
+let spillebrett = `
+
+╔═══╦═══╦═══╗
+║   ║   ║   ║
+╠═══╬═══╬═══╣
+║   ║   ║   ║
+╠═══╬═══╬═══╣
+║   ║   ║   ║
+╚═══╩═══╩═══╝
+`
+export {spillebrett};
